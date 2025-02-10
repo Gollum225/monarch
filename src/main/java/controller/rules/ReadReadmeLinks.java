8e9a213e12e230411cb511d85f6520c3ec6c18a8
Colin <colinbegero@gmail.com>
2025-02-10 18:40:34 +0100

    fix: Some websites took to long to respond

--- src/main/java/controller/rules/ReadReadmeLinks.java
+++ src/main/java/controller/rules/ReadReadmeLinks.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -125,12 +126,16 @@ public class ReadReadmeLinks extends Rule {
         URL url = new URL(websiteUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
+        connection.setConnectTimeout(60000);
+        connection.setReadTimeout(60000);
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 content.append(line).append("\n");
             }
+        } catch (SocketTimeoutException e) {
+            return "";
         }
 
         return content.toString();
