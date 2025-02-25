54448dcf4bc4e0cc8e11ae01116b60cf35b4fe64
Colin <colinbegero@gmail.com>
2025-02-25 18:37:38 +0100

    added Status window

--- src/main/java/controller/Checker.java
+++ src/main/java/controller/Checker.java
@@ -1,9 +1,13 @@
 package controller;
 
+import model.RuleReturn;
 import model.RepoList;
 import model.Repository;
+import util.Json;
+import view.Status;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -22,12 +26,19 @@ public class Checker {
      */
     private final RepoListManager listManager;
 
+    private int ruleAmount;
+
+    private Status status;
+
     /**
      * Creates a new Checker with the default rules.
      */
     public Checker() {
         rules = new RuleCollection();
         listManager = new RepoListManager(rules);
+        ruleAmount = rules.getRuleAmount();
+        status = new Status(ruleAmount);
+
     }
 
 
@@ -69,14 +80,18 @@ public class Checker {
                 Repository currentRepo = listManager.getNextRepo();
 
                 ArrayList<Rule> equippedRules = rules.equipRules(currentRepo);
+                status.addStatusBar(currentRepo);
                 for (Rule rule : equippedRules) {
                     if (rule.getType() == RuleType.MANDATORY) {
+                        status.updateStatusBar(currentRepo, rule.getClass().getSimpleName());
+
                         currentRepo.saveResult(rule.getClass(), rule.execute());
                     }
                 }
                 for (Rule rule : equippedRules) {
                     if (currentRepo.getOverallPoints() > 0) {
                         if (rule.getType() == RuleType.QUALITY) {
+                            status.updateStatusBar(currentRepo, rule.getClass().getSimpleName());
                             currentRepo.saveResult(rule.getClass(), rule.execute());
                         }
                     } else if (rule.getType() == RuleType.QUALITY) {
@@ -86,10 +101,16 @@ public class Checker {
 
                 listManager.finishRepo(currentRepo, rules);
 
+                status.removeStatusBar(currentRepo);
+
                 return null;
             });
         }
         return completionService;
+    }
+
+    private void finish() {
+        status.finish();
     }
 
 }
---
+++ src/main/java/view/Status.java
@@ -0,0 +1,110 @@
+package view;
+
+import model.Repository;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Responsible for creating a window with actual information regarding the progress.
+ */
+public class Status {
+    private final JFrame frame;
+    private final JPanel panel;
+    private final Map<Repository, JLabel> statusBars;
+    private final Map<Repository, Integer> progress = new HashMap<>();
+    private final int maxBlocks;
+
+    /**
+     * Creates a new Status window.
+     * @param steps how many individual processing steps each element has
+     */
+    public Status(int steps) {
+        frame = new JFrame("Status");
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        statusBars = new LinkedHashMap<>();
+
+        frame.add(panel);
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setVisible(true);
+        maxBlocks = steps;
+    }
+
+    /**
+     * Adds a new bar.
+     *
+     * @param repo {@link Repository} for the bar.
+     */
+    public void addStatusBar(Repository repo) {
+        if (statusBars.containsKey(repo)) {
+            return;
+        }
+        progress.putIfAbsent(repo, 0);
+
+        JLabel label = new JLabel(getProgressString(0) + " " + repo.getIdentifier() + ": ");
+        statusBars.put(repo, label);
+        panel.add(label);
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    /**
+     * Lets the bar progress one step further.
+     * @param repo to identify the right bar.
+     * @param ruleName to display the current processing rule.
+     */
+    public void updateStatusBar(Repository repo, String ruleName) {
+        JLabel label = statusBars.get(repo);
+        if (label != null) {
+            progress.replace(repo, progress.get(repo) + 1);
+
+            //String text = label.getText();
+            //int current = text.lastIndexOf("█") + 1; // Anzahl gefüllter Blöcke
+            //if (current < maxBlocks) {
+                label.setText(getProgressString(progress.get(repo)) + " " + ruleName + ": " + repo.getRepositoryName() + ", " + repo.getOwner());
+            //}
+        }
+        panel.revalidate();
+        panel.repaint();
+
+    }
+
+    /**
+     * Sets the bar of a repo to finish. It will be displayed in green.
+     *
+     * @param repo to finish
+     */
+    public void removeStatusBar(Repository repo) {
+        JLabel label = statusBars.get(repo);
+        if (label != null) {
+            label.setText(getProgressString(progress.get(repo)) + " " + repo.getRepositoryName() + ", " + repo.getOwner());
+            label.setForeground(new Color(0, 100, 0));
+        }
+    }
+
+    /**
+     * Closes the window.
+     */
+    public void finish() {
+        frame.dispose();
+    }
+
+    /**
+     * Calculates the progress of the bar.
+     *
+     * @param filled number of completed steps.
+     * @return the calculated String
+     */
+    private String getProgressString(int filled) {
+        return "█".repeat(2*filled) + "░".repeat(2*(maxBlocks - filled));
+    }
+
+}
