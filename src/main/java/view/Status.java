ad02beb5996dd7e8fc68c0bd928b2336faca3e33
Colin <colinbegero@gmail.com>
2025-02-28 12:48:06 +0100

    fix: not all statusbars worked because of concurrent access

--- src/main/java/view/Status.java
+++ src/main/java/view/Status.java
@@ -7,9 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.BoxLayout;
 import java.awt.Color;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Responsible for creating a window with actual information regarding the progress.
@@ -17,9 +16,9 @@ import java.util.Map;
 public class Status {
     private final JFrame frame;
     private final JPanel panel;
-    private JLabel finishedReposLabel;
+    private final JLabel finishedReposLabel;
     private final Map<Repository, JLabel> statusBars;
-    private final Map<Repository, Integer> progress = new HashMap<>();
+    private final Map<Repository, Integer> progress = new ConcurrentHashMap<>();
     private final int maxBlocks;
     private int finishedRepos = 0;
 
@@ -31,7 +30,7 @@ public class Status {
         frame = new JFrame("Status");
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        statusBars = new LinkedHashMap<>();
+        statusBars = new ConcurrentHashMap<>();
 
         frame.add(panel);
         frame.setSize(400, 300);
@@ -54,7 +53,7 @@ public class Status {
         if (statusBars.containsKey(repo)) {
             return;
         }
-        progress.putIfAbsent(repo, 0);
+        progress.put(repo, 0);
 
         JLabel label = new JLabel(getProgressString(0) + " " + repo.getIdentifier() + ": ");
         statusBars.put(repo, label);
@@ -78,11 +77,10 @@ public class Status {
                 return;
             }
 
-            //String text = label.getText();
-            //int current = text.lastIndexOf("█") + 1; // Anzahl gefüllter Blöcke
-            //if (current < maxBlocks) {
                 label.setText(getProgressString(progress.get(repo)) + " " + ruleName + ": " + repo.getRepositoryName() + ", " + repo.getOwner());
-            //}
+        }
+        else {
+            System.out.println("No status bar found for " + repo.getRepositoryName());
         }
         panel.revalidate();
         panel.repaint();
@@ -96,7 +94,7 @@ public class Status {
      */
     public void removeStatusBar(Repository repo) {
         JLabel label = statusBars.get(repo);
-        if (label != null) {
+        if (label != null && progress.get(repo) != null) {
             label.setText(getProgressString(progress.get(repo)) + " " + repo.getRepositoryName() + ", " + repo.getOwner());
             label.setForeground(new Color(0, 100, 0));
         }
@@ -110,6 +108,7 @@ public class Status {
      * Closes the window.
      */
     public void finish() {
+        frame.setVisible(false);
         frame.dispose();
     }
 
